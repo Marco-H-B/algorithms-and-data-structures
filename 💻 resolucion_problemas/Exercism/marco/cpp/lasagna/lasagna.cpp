2f26b44161e05d006234162794147020bd927b3e
Marco HB <marcoxhbx18@gmail.com>
2026-08-13 20:18:44 -0500

    FEAT(lasagna): resolver ejercicio lucian's luscious lasagna en c++

---
+++ 💻 resolucion_problemas/Exercism/marco/cpp/lasagna/lasagna.cpp
@@ -0,0 +1,20 @@
+// ovenTime returns the amount in minutes that the lasagna should stay in the oven.
+int ovenTime() {
+    int overTime{40};
+    return overTime;
+}
+
+// remainingOvenTime returns the remaining minutes based on the actual minutes already in the oven.
+int remainingOvenTime(int actualMinutesInOven) {
+    return ovenTime() - actualMinutesInOven;
+}
+
+// preparationTime returns an estimate of the preparation time based on the number of layers and the necessary time per layer.
+int preparationTime(int numberOfLayers) {
+    return 2 * numberOfLayers;
+}
+
+// elapsedTime calculates the total time spent to create and bake the lasagna so far.
+int elapsedTime(int numberOfLayers, int actualMinutesInOven) {
+    return preparationTime(numberOfLayers) + actualMinutesInOven;
+}
