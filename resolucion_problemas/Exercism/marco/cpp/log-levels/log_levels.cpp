030bd05ae797c6e60d7e4df4336ef5f6cc9c8ed5
Marco HB <marcoxhbx18@gmail.com>
2026-08-18 10:43:03 -0500

    STYLE(log-levels): agregar comentarios explicativos y formato a funciones de strings

--- resolucion_problemas/Exercism/marco/cpp/log-levels/log_levels.cpp
+++ resolucion_problemas/Exercism/marco/cpp/log-levels/log_levels.cpp
@@ -2,17 +2,20 @@
 
 // namespace log_line
 namespace log_line {
-std::string message(std::string line) {
-    int pos = line.find(": ");
-    return line.substr(pos + 2);
-}
+    // Recibe la línea de log y extrae solo el mensaje.
+    std::string message(std::string line) {
+        int pos = line.find(": ");
+        return line.substr(pos + 2);
+    }
 
-std::string log_level(std::string line) {
-    int pos = line.find("]");
-    return line.substr(1, pos - 1);
-}
+    // Recibe la línea de log y extrae el nivel de log.
+    std::string log_level(std::string line) {
+        int pos = line.find("]");
+        return line.substr(1, pos - 1);
+    }
 
-std::string reformat(std::string line) {
-    return message(line) + " (" + log_level(line) + ")";
-}
-} 
+    // Recibe la línea de log y la reformatea.
+    std::string reformat(std::string line) {
+        return message(line) + " (" + log_level(line) + ")";
+    }
+} // namespace log_line
